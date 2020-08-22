1e43e4e533524598d8885a0763471978d73ab0ae
apkumar1 <apkumar298@gmail.com>
2020-08-22 02:19:29 -0400

    added dashboard for emotion detection - credits to shards

---
+++ src/data/sidebar-nav-items.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    {
+      title: "Eyemage",
+      to: "/eyemage",
+      htmlBefore: '<i class="material-icons">edit</i>',
+      htmlAfter: ""
+    }
+  ];
+}
